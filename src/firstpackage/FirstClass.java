6cd3e3da2d38fdbd804a610aac69b58ea5ad7bc3
Saban Kucuk <sabanku34@gmail.com>
2020-06-30 14:24:13 -0400

    ikinci commit local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,5 +3,7 @@ package firstpackage;
 public class FirstClass {
 public static void main(String[] args) {
 	System.out.println("first commit from local repository");
+	System.out.println("second commit from local repository");
+
 }
 }
