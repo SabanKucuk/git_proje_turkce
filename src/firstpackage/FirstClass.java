0ab17a87709c888340087e97f6c5f96470ef5182
SabanKucuk <62161967+SabanKucuk@users.noreply.github.com>
2020-06-30 18:29:56 -0400

    Update FirstClass.java
    artik cozum ariyoruz

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 
 
 	 
-	System.out.println("conflict yapmak istiyoruz");
+	System.out.println("We are against the conflict");
 
 	System.out.println("first commit from remote repository");
 
